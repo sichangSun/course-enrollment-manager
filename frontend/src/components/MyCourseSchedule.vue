49532c688b9ee4d3e8c26cd54a4a9d364ec9abcd
Sichang Sun <krkhssc724@gmail.com>
2024-05-23 20:03:08 +0900

    Fixed MyCourseSchedule

--- frontend/src/components/MyCourseSchedule.vue
+++ frontend/src/components/MyCourseSchedule.vue
@@ -1,10 +1,13 @@
   <template>
     <v-app>
         <v-container>
+            <v-row>
+                <h3>時間割</h3>
+            </v-row>
             <!-- Header Row: Weekday-->
             <v-row>
                 <v-col cols="2"></v-col>
-                <v-col v-for="day in days" :key="index" cols="2">{{ day }}</v-col>
+                <v-col v-for="day in days" :key="day.index" cols="2">{{ day }}</v-col>
             </v-row>
             <!-- Main Rows: TimeSlots-->
             <v-row v-for="timeSlot in timeSlots" :key="timeSlot">
@@ -24,39 +27,16 @@
     </v-app>
 
   </template>
-  <script>
+  <script setup>
   import { reactive } from 'vue'
-  export default {
-    setup() {
-      const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-    //   const timeFrames=['9:00 ~ 10:30','10:40 ~ 12:10','13:10 ~ 14:40','14:50 ~ 16:20','16:30 ~ 18:00'];
-      const timeSlots = [
-        {name:'First',timeFrame:'9:00 ~ 10:30'},
-        {name:'Second',timeFrame:'10:40 ~ 12:10'},
-        {name:'Third',timeFrame:'13:10 ~ 14:40'},
-        {name:'Fourth',timeFrame:'14:50 ~ 16:20'},
-        {name:'Fifth',timeFrame:'16:30 ~ 18:00'}
-    ];
-      let gridData = reactive({
-        First: { Monday: 'Math', Tuesday: '', Wednesday: 'English', Thursday: '', Friday: ''},
-        Second: { Monday: '', Tuesday: '', Wednesday: '', Thursday: '', Friday: ''},
-        Third: { Monday: '', Tuesday: '', Wednesday: '', Thursday: '', Friday: ''},
-        Fourth: { Monday: 'Gym', Tuesday: 'Art', Wednesday: '', Thursday: '', Friday: '' },
-        Fifth: { Monday: '', Tuesday: '', Wednesday: '', Thursday: '', Friday: ''}
-      });
-      return {
-        days,
-        timeSlots,
-        gridData
-      };
-    }
-  }
+
+  const props = defineProps(['days','timeSlots','gridData'])
+
   </script>
 
   <style>
   .v-container {
     max-width: 1200px;
-    margin: auto;
   }
   .v-row {
     margin-bottom: 10px;
@@ -76,5 +56,8 @@
   }
   .v-card-title {
     font-size: 10px;
+  }
+  h3{
+    margin-top: -30px;
   }
   </style>
