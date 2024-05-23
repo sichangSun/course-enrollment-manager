32703eecc1cf2683589810975729ae95efbb7cbe
Sichang Sun <krkhssc724@gmail.com>
2024-05-24 00:44:58 +0900

    Finish BaseInfo components

---
+++ frontend/src/components/BaseInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+      <div class="base-box">
+        <div class="base-info">
+        <v-card>
+          <v-card-title>
+            {{  student.studentName }}
+          </v-card-title>
+          <v-card-subtitle>{{ student.studentInfo }}</v-card-subtitle>
+        </v-card>
+      </div>
+      <div class="base-regu">
+        <h3>{{ btn.btnTitle }}</h3>
+        <v-btn class="base-regu regu">{{btn.btnName}}</v-btn>
+      </div>
+      </div>
+  </main>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+const props = defineProps(['student','btn'])
+
+</script>
+<style>
+.base-box{
+  /* margin-top: 90px;
+    width: 600px;
+    margin-left: 60px; */
+    padding: 20px;
+    margin: 90px 20px auto;
+}
+.base-info{
+  width: 50%;
+    margin: auto 14%;
+}
+
+.base-regu{
+  position: absolute;
+    margin: auto 65%;
+    margin-top: -70px;
+}
+.regu{
+  margin: 30px;
+}
+</style>
