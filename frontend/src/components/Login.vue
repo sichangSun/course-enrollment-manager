ebb9f451bcd24adc7fd957b87889ddc9b8c768d2
Sichang Sun <krkhssc724@gmail.com>
2024-05-23 21:28:52 +0900

    Finish LoginView and LoginComponent with screen display only

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <!-- <v-app> -->
+    <v-container >
+      <div class="tittle">
+        <h2>
+          ログイン
+        </h2>
+      </div>
+      <div class="login-form">
+    <!-- <h2>Login</h2> -->
+    <v-form>
+      <v-row>
+        <v-col>
+          <v-text-field
+          v-model="state.email"
+          label="E-mail"
+          @blur="v$.email.$touch"
+          :error-messages ="v$.email.$errors.map((e) => e.$message)"
+          @input="v$.email.$touch">
+          </v-text-field>
+
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="state.password"
+            label="Password"
+            type="password"
+            @blur="v$.password.$touch"
+             :error-messages ="v$.password.$errors.map((e) => e.$message)"
+             @input="v$.password.$touch"
+            >
+          </v-text-field>
+
+        </v-col>
+      </v-row>
+        <div class="button" >
+          <v-btn type="submit" :disabled="v$.$invalid" color="primary" @click.prevent="login">Login</v-btn>
+        </div>
+    </v-form>
+  </div>
+    </v-container>
+  <!-- </v-app> -->
+</template>
+
+<script setup>
+import { reactive, computed, ref, watch} from 'vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, email,minLength } from '@vuelidate/validators'
+  let state = reactive({
+        email: '',
+        password:''
+  })
+  // validators setting
+  const rules = {
+        email: { required,email },
+        password: { required, minLengthValue: minLength(6)},
+  }
+  const v$ = useVuelidate(rules, state)
+  // validate data
+  function validateForm(){
+
+  }
+  //login
+  function login () {
+    validateForm()
+
+  }
+</script>
+<style>
+.v-container {
+    max-width: 1000px;
+  }
+.login-form {
+  padding: 30px 20px;
+  margin: auto;
+  width: 500px;
+}
+.button{
+  margin-top: 40px;
+}
+.tittle{
+  margin: auto;
+    padding: 10px 30%;
+    text-align: center;
+
+}
+</style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+  <Login class="login"></Login>
+  </v-app>
+
+</template>
+
+<script setup>
+  import Login from  '../components/Login.vue'
+  // import { VApp, VBtn } from 'vuetify/components';
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+
+    // function toCourse(){
+    //   console.log('OK')
+    //   $router.push({ name:'courseList'})
+    // }
+
+</script>
+<style>
+.login{
+  margin: 90px auto;
+}
+
+</style>
