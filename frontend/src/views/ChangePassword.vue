cfb516d774fae16894923b2339e669223fa7b4e4
Sichang Sun <krkhssc724@gmail.com>
2024-05-27 07:19:25 +0900

    Add ChangePassword view & compoment just screen display

---
+++ frontend/src/views/ChangePassword.vue
@@ -0,0 +1,32 @@
+<template>
+<BackToStudentHome/>
+  <ChangePassword
+  @change-password="ChangePassword"
+  class="change-password"/>
+</template>
+
+
+<script setup>
+import ChangePassword from '../components/ChangePassword.vue'
+import BackToStudentHome from '../components/BackToStudentHome.vue'
+
+
+// const ChangePassword = async (state,$v) => {
+//   if ($v.$invalid) {
+//       // errMsg.value=''
+//       return;
+//     }
+//     //set res
+//     const res={
+
+//     }
+//     // call API
+// }
+
+</script>
+
+<style>
+.change-password{
+  margin: 90px auto;
+}
+</style>
